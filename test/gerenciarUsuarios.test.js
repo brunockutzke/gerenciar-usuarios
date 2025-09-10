2a2a84e79696bb0f35aff5877b7857bccca6bdf0
brunockutzke <brunokutzke@gmail.com>
2025-09-09 22:56:15 -0300

    Mudei a lista de nomes de usuários para que se tornasse uma lista de objetos.

--- test/gerenciarUsuarios.test.js
+++ test/gerenciarUsuarios.test.js
@@ -5,10 +5,13 @@ describe('Testar as funções de gestão de usuários', () => {
 
     it('Validar que posso adicionar um novo nome de usuário na lista', () => {
         //1. Adicionar um novo nome na lista de usuários
-        adicionarNovoUsuario('Bruno');
+        adicionarNovoUsuario({
+            nome: 'Maria',
+            email: 'maria@gospel.com'
+        });
         //2. Retornar a lista de usuários na caixa lista de usuários
         const listaDeUsuarios = retornarUsuarios();
         //3. Comparar se o novo nome está no fim da lista de usuários
-        expect(listaDeUsuarios.at(-1)).to.equal('Bruno')
+        expect(listaDeUsuarios.at(-1).nome).to.equal('Maria')
     })
 })
